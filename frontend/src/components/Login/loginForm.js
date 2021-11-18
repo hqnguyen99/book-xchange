a31588b40ac1ef2da5295104d22332f380f9656a
hqnguyen <hqnguyen@sfu.ca>
2021-11-18 12:45:34 -0800

    Change login param

--- frontend/src/components/Login/loginForm.js
+++ frontend/src/components/Login/loginForm.js
@@ -1,11 +1,11 @@
-import React, { useRef, useContext} from 'react'
+import React, { useRef, useContext } from 'react'
 import styled from 'styled-components'
 import ButtonOne from '../button/ButtonOne'
 import ButtonTwo from '../button/ButtonTwo';
 import AuthContext from '../../store/auth-context';
 import Url from '../../store/url';
 
-const MiddleOfPage= styled.div`
+const MiddleOfPage = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -16,63 +16,63 @@ const MiddleOfPage= styled.div`
     transform: translate(-50%, -50%);
 `;
 
-export default function LoginForm({navigateHandler}) {
-    const loginEndpoint= Url.login_url;
-    const loginIdInputRef = useRef();
-    const passwordInputRef = useRef();
-    const authCtx= useContext(AuthContext)
-    
-    const handleSubmit = async e => {
-        e.preventDefault();
-        const enteredLoginId = loginIdInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-        fetch(loginEndpoint,{
-            method: 'POST',
-            body: JSON.stringify({
-                email: enteredLoginId,
-                password: enteredPassword,
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then((res) => {
-            if (res.ok) {
-              return res.json();
-            } else {
-              return res.json().then((data) => {
-                let errorMessage = 'Authentication failed!';
-                // if (data && data.error && data.error.message) {
-                //   errorMessage = data.error.message;
-                // }
-    
-                throw new Error(errorMessage);
-              });
-            }
-          })
-          .then((data) => {
-            authCtx.login(data.accessToken);
-            navigateHandler()
-          })
-          .catch((err) => {
-            alert(err.message);
-          });
-      };
-    return(
-        <MiddleOfPage>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <p>Login Id*</p>
-            <input type="text" id='login_id' required ref={loginIdInputRef}/>
-          </label>
-          <label>
-            <p>Password*</p>
-            <input type="password" required ref={passwordInputRef}/>
-          </label>
-          <div>
-            <ButtonOne type="submit" title ="Login"></ButtonOne>
-          </div>
-        </form>
-            <ButtonTwo onClick={navigateHandler} title= "Login as Guest"/> 
-        </MiddleOfPage>
-      )
+export default function LoginForm({ navigateHandler }) {
+  const loginEndpoint = Url.login_url;
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const authCtx = useContext(AuthContext)
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    fetch(loginEndpoint, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = 'Authentication failed!';
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+
+          throw new Error(errorMessage);
+        });
+      }
+    })
+      .then((data) => {
+        authCtx.login(data.accessToken);
+        navigateHandler()
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+  return (
+    <MiddleOfPage>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Email*</p>
+          <input type="text" id='email' required ref={emailInputRef} />
+        </label>
+        <label>
+          <p>Password*</p>
+          <input type="password" required ref={passwordInputRef} />
+        </label>
+        <div>
+          <ButtonOne type="submit" title="Login"></ButtonOne>
+        </div>
+      </form>
+      <ButtonTwo onClick={navigateHandler} title="Login as Guest" />
+    </MiddleOfPage>
+  )
 }
