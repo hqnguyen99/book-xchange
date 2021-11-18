35d603204257b076302d3a353983518610f46d20
springbro294 <xanh294@gmail.com>
2021-11-18 01:37:07 -0800

    new page on click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/pages/login';
 import Home from './components/pages/home';
 import Signup from './components/pages/signup';
 import SellPage from './components/pages/sellBook';
+import BuyPage from './components/pages/buyBook';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/sell" element={<SellPage />}></Route>
+          <Route path="/info" element={<BuyPage />}></Route>
         </Routes>
       </div>
     </Router>
