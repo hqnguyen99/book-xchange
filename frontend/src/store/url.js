aa158e888cf000d8f72164bb169da4e86a072acc
hqnguyen <hqnguyen@sfu.ca>
2021-11-18 17:32:29 -0800

    update cors

--- frontend/src/store/url.js
+++ frontend/src/store/url.js
@@ -1,6 +1,6 @@
-const url= 'https://cors-everywhere.bookxchangebackend-env.eba-g2sbc6cf.us-east-1.elasticbeanstalk.com';
+const url= 'https://young-island-58664.herokuapp.com/http://bookxchangebackend-env.eba-g2sbc6cf.us-east-1.elasticbeanstalk.com';
 const Url= {
-    
+    home_url: '',
     login_url: url + '/auth/login',
     signup_url: url + '/user/signup',
     refreshToken_url: url+ '/auth/refreshToken'
