2c3583c966c1e3ef724fe3c5e694b63b05c887e3
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2021-12-01 14:32:46 -0800

    Update url.js

--- frontend/src/store/url.js
+++ frontend/src/store/url.js
@@ -1,4 +1,4 @@
-const url= 'https://young-island-58664.herokuapp.com/http://ec2co-ecsel-iomsfa68gt8u-758619912.us-east-1.elb.amazonaws.com:5000/';
+const url= 'https://young-island-58664.herokuapp.com/http://ec2co-ecsel-iomsfa68gt8u-758619912.us-east-1.elb.amazonaws.com:5000';
 const Url= {
     home_url: '',
     login_url: url + '/auth/login',
