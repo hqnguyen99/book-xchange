8f9aec7ae05c90afe716e9d339a528b3a1eaf6ae
minh220399 <60809722+minh220399@users.noreply.github.com>
2021-12-02 01:13:04 -0800

    Update url.js

--- frontend/src/store/url.js
+++ frontend/src/store/url.js
@@ -1,7 +1,8 @@
 const url= 'https://young-island-58664.herokuapp.com/EC2Co-EcsEl-IOMSFA68GT8U-758619912.us-east-1.elb.amazonaws.com:99';
+const url1= 'https://young-island-58664.herokuapp.com/EC2Co-EcsEl-IOMSFA68GT8U-758619912.us-east-1.elb.amazonaws.com:5000';
 const Url= {
     home_url: '',
-    login_url: url + '/auth/login',
+    login_url: url1 + '/auth/login',
     signup_url: url + '/user/signup',
     refreshToken_url: url+ '/auth/refreshToken'
 };
