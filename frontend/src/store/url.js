553973171fff2a1fda308073e05ade0724dd6438
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2021-12-02 23:16:28 -0800

    Update url.js

--- frontend/src/store/url.js
+++ frontend/src/store/url.js
@@ -1,14 +1,13 @@
 const proxy_server_url= 'https://young-island-58664.herokuapp.com/'
-const url= proxy_server_url+ 'EC2Co-EcsEl-IOMSFA68GT8U-758619912.us-east-1.elb.amazonaws.com:99';
-const url_book= proxy_server_url+ 'EC2Co-EcsEl-IOMSFA68GT8U-758619912.us-east-1.elb.amazonaws.com:3300';
-const url_user= proxy_server_url+ 'EC2Co-EcsEl-IOMSFA68GT8U-758619912.us-east-1.elb.amazonaws.com:5000';
+const url= proxy_server_url+ 'http://api-gateway-837901308.us-east-1.elb.amazonaws.com:99';
 const Url= {
     home_url: '',
     login_url: url + '/auth/login',
-    signup_url: url_user + '/user/signup',
+    signup_url: url + '/user/signup',
     refreshToken_url: url+ '/auth/refreshToken',
-    book_url: url_book + '/books/all',
-    add_book: url_book + '/books/addBook',
-    book_by_ID: url_book + '/books/byID'
+    book_url: url + '/books/all',
+    add_book: url + '/books/addBook',
+    book_by_ID: url + '/books/byID'
 };
 export default Url;
+
