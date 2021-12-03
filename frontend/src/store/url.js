46b02a25a29558972f22a5b7ee69561910f7c68e
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2021-12-02 18:55:21 -0800

    Update url.js

--- frontend/src/store/url.js
+++ frontend/src/store/url.js
@@ -1,5 +1,5 @@
 const proxy_server_url= 'https://young-island-58664.herokuapp.com/'
-const url= proxy_server_url+ 'http://apigateway-457266473.us-east-1.elb.amazonaws.com:99';
+const url= proxy_server_url+ 'api-gateway-440942407.us-east-1.elb.amazonaws.com:99';
 const Url= {
     home_url: '',
     login_url: url + '/auth/login',
